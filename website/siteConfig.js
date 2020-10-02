916434b9ea75a7218b99f559cb85dc066c0e82af
umarethina <uma.rethina@kaybus.com>
2020-10-02 12:18:34 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Uma R Smith', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://umarethina.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'umarsmith',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -69,6 +69,7 @@ const siteConfig = {
     socialMedia: {
         linkedin: 'https://www.linkedin.com/in/umarsmith/',
         github: 'https://github.com/umarethina',
+        twitter: ''
     },
 
     // You may provide arbitrary config keys to be used as needed by your
